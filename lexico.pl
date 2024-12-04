cc8f360f76e2e591c7bb2cd91248d94b57e575c8
lucasfs1007 <lucasfs1007@gmail.com>
2024-12-04 14:34:20 -0300

    adiciona base com testes e como rodar o que foi feito

--- lexico.pl
+++ lexico.pl
@@ -1,41 +1,61 @@
-% Define os tokens da gramática.
-token(void, keyword).
-token(char, keyword).
-token(int, keyword).
-token(main, keyword).
-token(if, keyword).
-token(else, keyword).
-token(while, keyword).
-token(return, keyword).
-token(break, keyword).
-token(true, constant).
-token(false, constant).
-token("(", lparen).
-token(")", rparen).
-token("{", lbrace).
-token("}", rbrace).
-token("[", lbracket).
-token("]", rbracket).
-token(";", semicolon).
-token(",", comma).
-token(":", colon).
-token("=", assignop).
-token("+=", assignop).
-token("-=", assignop).
-token("*=", assignop).
-token("/=", assignop).
-token("==", relop).
-token("!=", relop).
-token("<", relop).
-token("<=", relop).
-token(">", relop).
-token(">=", relop).
-token("+", sumop).
-token("-", sumop).
-token("*", mulop).
-token("/", mulop).
-token("%", mulop).
-token("&&", andop).
-token("||", orop).
-token("!", notop).
+% tokens
+token(void, 'void').
+token(char, 'char').
+token(int, 'int').
+token(main, 'main').
+token(void, 'void').
+token(left_paren, '(').
+token(right_paren, ')').
+token(left_brace, '{').
+token(right_brace, '}').
+token(semicolon, ';').
+token(comma, ',').
+token(colon, ':').
+token(assign, '=').
+token(add_assign, '+=').
+token(sub_assign, '-=').
+token(mul_assign, '*=').
+token(div_assign, '/=').
+token(if, 'if').
+token(else, 'else').
+token(while, 'while').
+token(return, 'return').
+token(break, 'break').
+token(increment, '++').
+token(decrement, '--').
+token(or, '||').
+token(and, '&&').
+token(not, '!').
+token(relop('<'), '<').
+token(relop('>'), '>').
+token(relop('=='), '==').
+token(relop('!='), '!=').
+token(relop('>='), '>=').
+token(relop('<='), '<=').
+token(sumop('+'), '+').
+token(sumop('-'), '-').
+token(mulop('*'), '*').
+token(mulop('/'), '/').
+token(mulop('%'), '%').
+token(unaryop('-'), '-').
 
+% defincao de ids e constantes
+token(id(ID), ID) :- atom_chars(ID, [C|_]), char_type(C, alpha).
+token(numconst(NUM), NUM) :- number(NUM).
+token(charconst(CHAR), CHAR) :- atom_chars(CHAR, ['\'', _ , '\'']).
+token(true, 'true').
+token(false, 'false').
+
+% Tokenize uma lista de átomos (código de entrada)
+tokenize([], []).
+tokenize([H|T], [Token|Tokens]) :-
+    token(Token, H),
+    tokenize(T, Tokens).
+
+% execute: swpl
+% tokenize(['int', 'main', '(', ')', '{', 'return', ';', '}'], Tokens).
+% roda os testes...deixei um exemplo
+
+% teste:
+% usa essa entrada aqui:tokenize(['int', 'main', '(', ')', '{', 'return', ';', '}'], Tokens).
+% ele vai retornar isso:  Tokens = [int, main, left_paren, right_paren, left_brace, return, semicolon, right_brace].
