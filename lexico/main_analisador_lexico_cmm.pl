a648ec66517a85156460473e2bec2fc50ba0af1e
Leonardo Gonçalves Machado <leonardogm2003@gmail.com>
2024-12-09 17:46:50 -0300

    imlementando parser sintatico ate stmt
    co-authored-by: Bruno Ribeiro <bbrunoo@icloud.com>
    co-authored-by: Igor Penha <igor.penharol@gmail.com>

--- lexico/main_analisador_lexico_cmm.pl
+++ lexico/main_analisador_lexico_cmm.pl
@@ -1,16 +1,20 @@
 :- consult('analisador_lexico_cmm.pl').
+:- consult('../sintatico/parser.pl').
 
 % Função principal para processar e exibir tokens
 main :-
     writeln('=== Iniciando Analisador Léxico ==='),
     % Exemplo de código para análise
-    Codigo = ['int', 'main', '(', ')', '{', '/*', '*/' ,'int', 'x', '=', '5', ';', 'if', '(', 'x', '<', '10', ')', '{', 'x', '+=', '1', ';', '}', '}'],
+    %Codigo = ['int', 'main', '(', ')', '{', '/*', '*/' ,'int', 'x', '=', '5', ';', 'if', '(', 'x', '<', '10', ')', '{', 'x', '+=', '1', ';', '}', '}'],
+    Codigo = ['int', 'main', '(', 'void', ')', '{','int', 'rei', ';','}'],
     writeln('Código para análise:'),
     writeln(Codigo),
     writeln('Tokenizando...'),
     tokenizar(Codigo, Tokens),
     writeln('Tokens Gerados:'),
-    writeln(Tokens).
+    writeln(Tokens),
+    writeln('=== Analise Sintatica ==='),
+
+    parser(Tokens).
 
 % Executa a função principal ao carregar o arquivo
-:- main.
