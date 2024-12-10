73d493e33a8721e28276ff3560b6cbaf53c86972
gitbmvb <bruno.martval@gmail.com>
2024-12-10 02:28:04 -0300

    tentando integrar os analisadores léxico e sintático na main

---
+++ main.pl
@@ -0,0 +1,29 @@
+:- consult('lexico/analisador_lexico_cmm.pl').
+:- consult('sintatico/analisador_sintatico.pl').
+
+% Função principal para processar e exibir tokens
+% Para executar o compilador, basta executar swipl -s main.pl -g main -- codigo.cmm
+main :-
+    current_prolog_flag(argv, Args),
+    ( Args = [Filename|_] ->
+        writeln('=== Iniciando Analisador Léxico ==='),
+        writeln('Lendo código do arquivo...'),
+        read_file(Filename, Codigo),
+        writeln('Código para análise:'),
+        writeln(Codigo),
+        writeln('Tokenizando...'),
+        tokenizar(Codigo, Tokens),
+        writeln('Tokens Gerados:'),
+        writeln(Tokens),
+        writeln('=== Analise Sintática ==='),
+        parser(Tokens)
+    ; writeln('Erro: Nenhum arquivo fornecido como argumento.'), halt(1)
+    ).
+
+% Função para ler o conteúdo do arquivo e convertê-lo em uma lista de palavras
+read_file(Filename, Codigo) :-
+    open(Filename, read, Stream),
+    read_string(Stream, _, String),
+    close(Stream),
+    split_string(String, " \t\n", " \t\n", Words),
+    maplist(atom_string, Codigo, Words).
