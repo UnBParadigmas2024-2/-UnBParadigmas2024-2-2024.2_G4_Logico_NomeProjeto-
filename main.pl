5a5346527acf03618d65c6c21b9a0e49f7f720bf
gitbmvb <bruno.martval@gmail.com>
2024-12-11 01:39:12 -0300

    integra as duas análises na main

--- main.pl
+++ main.pl
@@ -2,19 +2,28 @@
 :- consult('sintatico/analisador_sintatico.pl').
 
 % Função principal para processar e exibir tokens
+% Para executar o compilador, basta executar swipl -s main.pl -g main -- codigo.cmm
 main :-
-    writeln('=== Iniciando Analisador Léxico ==='),
-    % Exemplo de código para análise
-    %Codigo = ['int', 'main', '(', ')', '{', '/*', '*/' ,'int', 'x', '=', '5', ';', 'if', '(', 'x', '<', '10', ')', '{', 'x', '+=', '1', ';', '}', '}'],
-    Codigo = ['int', 'main', '(', 'void', ')', '{','int', 'rei', ';','}'],
-    writeln('Código para análise:'),
-    writeln(Codigo),
-    writeln('Tokenizando...'),
-    tokenizar(Codigo, Tokens),
-    writeln('Tokens Gerados:'),
-    writeln(Tokens),
-    writeln('=== Analise Sintatica ==='),
+    current_prolog_flag(argv, Args),
+    ( Args = [Filename|_] ->
+        writeln('=== Iniciando Analisador Léxico ==='),
+        writeln('Lendo código do arquivo...'),
+        read_file(Filename, Codigo),
+        writeln('Código para análise:'),
+        writeln(Codigo),
+        writeln('Tokenizando...'),
+        tokenizar(Codigo, Tokens),
+        writeln('Tokens Gerados:'),
+        writeln(Tokens),
+        writeln('=== Analise Sintática ==='),
+        parser(Tokens)
+    ; writeln('Erro: Nenhum arquivo fornecido como argumento.'), halt(1)
+    ).
 
-    parser(Tokens).
-
-% Executa a função principal ao carregar o arquivo
+% Função para ler o conteúdo do arquivo e convertê-lo em uma lista de palavras
+read_file(Filename, Codigo) :-
+    open(Filename, read, Stream),
+    read_string(Stream, _, String),
+    close(Stream),
+    split_string(String, " \t\n", " \t\n", Words),
+    maplist(atom_string, Codigo, Words).
