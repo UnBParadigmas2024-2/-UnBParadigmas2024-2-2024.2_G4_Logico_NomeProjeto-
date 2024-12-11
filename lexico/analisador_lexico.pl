99a8ccbf588138ed42c34164ae44d1dcec459687
heitormsb <heitormsb@gmail.com>
2024-12-11 19:28:59 -0300

    ajusta lexico para diferenciar numconst e id

--- lexico/analisador_lexico.pl
+++ lexico/analisador_lexico.pl
@@ -7,10 +7,16 @@ analisa(Input, Tokens) :-
 
 analisa_charlist([], []).
 analisa_charlist([Char|Resto], Tokens) :-
-    char_type(Char, space), !,
+    char_type(Char, space), !, % Ignorar espaços
+    analisa_charlist(Resto, Tokens).
+analisa_charlist(Lista, [Token|Tokens]) :-
+    pega_numero(Lista, Numero, Resto),
+    Numero \= [],
+    number_codes(Num, Numero),
+    Token = numconst(Num),
     analisa_charlist(Resto, Tokens).
 analisa_charlist([Char, Next|Resto], [Token|Tokens]) :-
-    atom_codes(Operador, [Char, Next]),
+    atom_codes(Operador, [Char, Next]), % Verifica operadores compostos (ex.: <=, >=)
     token(Token, Operador), !,
     analisa_charlist(Resto, Tokens).
 analisa_charlist([Char|Resto], [Token|Tokens]) :-
@@ -22,23 +28,18 @@ analisa_charlist(Lista, [Token|Tokens]) :-
     atom_codes(Ident, Identificador),
     (token(Token, Ident) -> true; Token = id(Ident)),
     analisa_charlist(Resto, Tokens).
-analisa_charlist(Lista, [Token|Tokens]) :-
-    pega_numero(Lista, Numero, Resto),
-    atom_codes(Num, Numero),
-    Token = numero(Num),
-    analisa_charlist(Resto, Tokens).
 
 % Função auxiliar para pegar identificadores
 pega_identificador([Char|Resto], [Char|IdentificadorResto], RestoFinal) :-
-    char_type(Char, alpha), !,
+    char_type(Char, alpha), !, % Letras
     pega_identificador(Resto, IdentificadorResto, RestoFinal).
 pega_identificador([Char|Resto], [Char|IdentificadorResto], RestoFinal) :-
-    char_type(Char, digit), !,
+    char_type(Char, digit), !, % Números dentro de identificadores
     pega_identificador(Resto, IdentificadorResto, RestoFinal).
 pega_identificador(Lista, [], Lista).
 
 % Função auxiliar para pegar números
 pega_numero([Char|Resto], [Char|NumeroResto], RestoFinal) :-
-    char_type(Char, digit), !,
+    char_type(Char, digit), !, % Apenas dígitos
     pega_numero(Resto, NumeroResto, RestoFinal).
 pega_numero(Lista, [], Lista).
