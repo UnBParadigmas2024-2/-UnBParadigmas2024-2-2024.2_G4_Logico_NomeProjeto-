9b9c03be4e2aa490ae50f57b9553c05214f89dd8
heitormsb <heitormsb@gmail.com>
2024-12-12 00:26:07 -0300

    ajusta lexico quando tem _ na funcao e retornando erro

--- lexico/analisador_lexico.pl
+++ lexico/analisador_lexico.pl
@@ -16,7 +16,7 @@ analisa_charlist(Lista, [Token|Tokens]) :-
     Token = numconst(Num),
     analisa_charlist(Resto, Tokens).
 analisa_charlist([Char, Next|Resto], [Token|Tokens]) :-
-    atom_codes(Operador, [Char, Next]), % Verifica operadores compostos (ex.: <=, >=)
+    atom_codes(Operador, [Char, Next]), % Verifica operadores compostos
     token(Token, Operador), !,
     analisa_charlist(Resto, Tokens).
 analisa_charlist([Char|Resto], [Token|Tokens]) :-
@@ -25,16 +25,22 @@ analisa_charlist([Char|Resto], [Token|Tokens]) :-
     analisa_charlist(Resto, Tokens).
 analisa_charlist(Lista, [Token|Tokens]) :-
     pega_identificador(Lista, Identificador, Resto),
+    Identificador \= [],
     atom_codes(Ident, Identificador),
     (token(Token, Ident) -> true; Token = id(Ident)),
     analisa_charlist(Resto, Tokens).
+analisa_charlist([Char|_], _) :- % Tratamento de erro
+    \+ char_type(Char, space), % Não é espaço
+    \+ char_type(Char, alpha), % Não é alfabético
+    \+ char_type(Char, digit), % Não é dígito
+    atom_codes(InvalidChar, [Char]),
+    format("Erro léxico: caractere inválido encontrado -> ~w~n", [InvalidChar]),
+    fail.
 
 % Função auxiliar para pegar identificadores
 pega_identificador([Char|Resto], [Char|IdentificadorResto], RestoFinal) :-
-    char_type(Char, alpha), !, % Letras
-    pega_identificador(Resto, IdentificadorResto, RestoFinal).
-pega_identificador([Char|Resto], [Char|IdentificadorResto], RestoFinal) :-
-    char_type(Char, digit), !, % Números dentro de identificadores
+    (char_type(Char, alpha) ; char_type(Char, digit) ; Char == 95),
+    !,
     pega_identificador(Resto, IdentificadorResto, RestoFinal).
 pega_identificador(Lista, [], Lista).
 
